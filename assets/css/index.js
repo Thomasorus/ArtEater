f6ebc6b58fc7083bd276bba864bfa816ebc51476
Thomasorus <contact@thomasorus.com>
2020-03-08 23:13:38 +0100

    Updated styles

--- assets/css/index.js
+++ assets/css/index.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56114" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61652" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
