eae73db60c2d84eba76aae94010488754d598fed
Thomasorus <contact@thomasorus.com>
2020-03-17 11:43:13 +0100

    Updated header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,14 +9,11 @@
 
 <!doctype html>
 <html lang="en">
-
 <head>
-
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
   <title><?= $site->title() ?> | <?= $page->title() ?></title>
   <?= css(['assets/css/index.css', '@auto']) ?>
-
 
 
   <script>
