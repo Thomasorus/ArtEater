8f51ede15f1bfe8ec64812542c74230bb64b7369
Thomasorus <contact@thomasorus.com>
2022-01-01 16:53:14 +0100

    Updated about page

--- site/templates/about.php
+++ site/templates/about.php
@@ -2,30 +2,33 @@
 <main>
     <div class="post-title">
       <h1 class="post-title__text"><?= $page->title() ?></h1>
-      
+
     </div>
     <div class="post content-box">
-      
       <?= $page->text()->kt() ?>
-    
-    <?php if($authors != null): ?>
-    <h2>The Team</h2>
-    <?php foreach ($authors as $author): ?>
-      <div class="author">
-           <h3><?= $author->name(); ?></h3>
 
-        <div class="author-pic">
-          <?= $author->photo()->toFile(); ?>  
-        </div>
-        <div>
-         
-          <?= $author->bio()->kt(); ?>
+      <?php if($authors != null): ?>
+        <h2>Meet the team</h2>
+        <?php foreach ($authors as $author): ?>
+          <div class="author">
+            <h3><?= $author->name(); ?></h3>
+            <div class="author-bio">
+                <?php if($photo = $author->photo()->toFile()): ?>
+                  <img class="blend" src="<?= $photo->url(); ?>" alt="<?= $author->name(); ?>">
+                <?php endif ?>
+              <div>
+                <?= $author->bio()->kt(); ?>
+              </div>
+            </div>
+
+          <div>
         </div>
       </div>
-    <?php endforeach ?>
-  <?php endif ?>
+      <?php endforeach ?>
+    <?php endif ?>
+    <?= $page->promo()->kt() ?>
     </div>
-    
-  
+
+
 </main>
 <?php snippet('footer') ?>
